627e056df348829e6dd2d001b6b722876ceea4c2
VeliGultekin <120390428+VeliGultekin@users.noreply.github.com>
2023-11-01 19:22:08 +0300

    Add files via upload

---
+++ C#/Iki-tarih-arasindaki-gun/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Ilk tarihi GG/AA//YYYY seklinde giriniz!");
+        if (DateTime.TryParse(Console.ReadLine(), out var date1))
+        {
+            Console.WriteLine("Ikinci tarihi GG/AA//YYYY seklinde giriniz!");
+            if (DateTime.TryParse(Console.ReadLine(), out var date2))
+            {
+                int gunFarki = (int)Math.Abs((date1 - date2).TotalDays);
+                Console.WriteLine($"Aradaki gun farki: {gunFarki}");
+            }
+            else
+            {
+                Console.WriteLine("Lutfen date2'yi gecerli formatta bir tarih giriniz!");
+            }
+        }
+
+        else
+        {
+            Console.WriteLine("Lutfen date1'i gecerli formatta bir tarih giriniz!");
+        }
+        
+    }
+}
